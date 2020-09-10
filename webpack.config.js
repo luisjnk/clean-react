615a43441e6b9cf1acd20785d2380835832704b9
luis junqueira <ljunqueira@growin.com>
2020-09-10 21:13:13 +0100

    Add login page

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,48 @@
-import path from 'path';
-import { CleanWebpackPlugin } from 'clean-webpack-plugin'
+const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 
 module.exports = {
-  node: 'development',
-  entry: './src/main/index.tsx',
-  output: {
-    path: path.join(__dirname, 'public/js'),
-    publicPath: '/public/js',
-    filename: 'bundle.js'
+    entry: './src/main/index.tsx',
+    output: {
+        path: path.join(__dirname, 'public/js'),
+        publicPath: '/public/js',
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', 'scss'],
+        'alias': {
+            '@': path.join(__dirname, 'src')
+        }
+    },
+    module: {
+      rules: [{
+          test: /\.ts(x?)$/,
+          loader: 'ts-loader',
+          exclude: /node_modules/
+      }, {
+          test: /\.scss$/,
+          use: [{
+              loader: 'style-loader'
+          }, {
+              loader: 'css-loader',
+              options: {
+                  modules: true
+              }
+          }, {
+              loader: 'sass-loader'
+          }]
+      }]
   },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js'],
-    'alias': {
-      '@': path.join(__dirname, 'src')
-    }
-  },
-  devServer: {
-    contentBase: './public',
-    writeToDisk: true,
-    historyApiFallback: true
-  },
-  externals: {
-    react: 'React',
-    'react-dom': 'ReactDOM'
-  },
-  plugins: [
-      new CleanWebpackPlugin()
-  ]
+    devServer: {
+        contentBase: './public',
+        writeToDisk: true,
+        historyApiFallback: true
+    },
+    externals: {
+        react: 'React',
+        'react-dom': 'ReactDOM'
+    },
+    plugins: [
+        new CleanWebpackPlugin()
+    ]
 }
