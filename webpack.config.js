932380151225b54aa1971919137a94dcd1d9d09c
luis junqueira <ljunqueira@growin.com>
2020-09-10 19:47:57 +0100

    Add and configure webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,30 @@
+import path from 'path';
+import { CleanWebpackPlugin } from 'clean-webpack-plugin'
+
 module.exports = {
   node: 'development',
-  entry: './src'
+  entry: './src/main/index.tsx',
+  output: {
+    path: path.join(__dirname, 'public/js'),
+    publicPath: '/public/js',
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+    'alias': {
+      '@': path.join(__dirname, 'src')
+    }
+  },
+  devServer: {
+    contentBase: './public',
+    writeToDisk: true,
+    historyApiFallback: true
+  },
+  externals: {
+    react: 'React',
+    'react-dom': 'ReactDOM'
+  },
+  plugins: [
+      new CleanWebpackPlugin()
+  ]
 }
