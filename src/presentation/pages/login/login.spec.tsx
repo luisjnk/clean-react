153c622fbfa5a56c9ac318ed753d4dc8351fa7c1
luis junqueira <luisjnk@gmail.com>
2020-09-30 10:39:11 +0100

    Add indeity-obj-proxy

---
+++ src/presentation/pages/login/login.spec.tsx
@@ -0,0 +1,10 @@
+// __tests__/fetch.test.js
+import React from 'react'
+import { render, fireEvent, waitFor, screen } from '@testing-library/react'
+import Login from './login';
+
+describe('Login tests', () => {
+    test('', () => {
+        render(<Login />)
+    })
+})
