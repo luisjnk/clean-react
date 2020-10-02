159a0b3d6e6b3a0b0b2bc3c2e504d4b7ecf5f2b1
luis junqueira <luisjnk@gmail.com>
2020-10-02 17:19:19 +0100

    Refactor: move render to factory

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,12 +1,24 @@
 // __tests__/fetch.test.js
 import React from 'react'
-import { render, fireEvent, waitFor, screen } from '@testing-library/react'
+import { render, RenderResult, waitFor, screen } from '@testing-library/react'
 import Login from './login';
 import { ERROR_MESSAGES } from '@/presentation/utils/contants';
 
+type SutTypes = {
+  sut: RenderResult
+}
+
+const makeSut = (): SutTypes => {
+  const sut = render(<Login />)
+  return {
+    sut
+  }
+}
 describe('Login tests', () => {
   test('Ensure not render spinner and error on start', () => {
-    const { getByTestId } = render(<Login />)
+    const { sut } = makeSut()
+    const { getByTestId } = sut
+
     const errorWrap = getByTestId('error-wrap')
     expect(errorWrap.childElementCount).toBe(0)
   })
@@ -19,8 +31,9 @@ describe('Login tests', () => {
 
 
   test('Ensure login button is disabled', () => {
-    const { getByTestId } = render(<Login />)
-
+    const { sut } = makeSut()
+    const { getByTestId } = sut
+    
     const emailStatus = getByTestId('email-status')
     expect(emailStatus.title).toBe(ERROR_MESSAGES.REQUIRED_FIELD)
 
