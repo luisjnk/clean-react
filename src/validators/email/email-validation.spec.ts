5930ca4a721c7a5dca728c56b6b051f70ff999d1
Luís Junqueira <ljunqueira@Growins-MacBook-Pro.local>
2020-11-19 12:42:30 +0000

    Create email validation

--- src/validators/email/email-validation.spec.ts
+++ src/validators/email/email-validation.spec.ts
@@ -1,11 +1,17 @@
 import { InvalidFieldError } from "../errors"
 import { EmailValidation } from "./email.validation"
-
+import faker from "faker"
 describe('EmailValidation', () => {
   test('Should return error if email is invalid', () => {
     const sut = new EmailValidation("email")
-    const error = sut.validate('')
+    const error = sut.validate(faker.random.word())
     expect(error).toEqual(new InvalidFieldError())
+  })
+
+  test('Should error to be false', () => {
+    const sut = new EmailValidation("email")
+    const error = sut.validate(faker.internet.email())
+    expect(error).toBeFalsy()
   })
 
 })
--- src/validators/email/email.validation.ts
+++ src/validators/email/email.validation.ts
@@ -4,6 +4,7 @@ import { FieldValidation } from "../protocols/field-validation";
 export class EmailValidation implements FieldValidation {
     constructor(readonly field: string) { }
     validate(value: string): Error {
-      return new InvalidFieldError()
+        const emailregex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return emailregex.test(value) ? null : new InvalidFieldError()
     }
   }
