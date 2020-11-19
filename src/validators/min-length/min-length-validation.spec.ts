95906c297fe9a800f5ffa55701691a5bcce01517
Luís Junqueira <ljunqueira@Growins-MacBook-Pro.local>
2020-11-19 12:56:54 +0000

    move min lenght to constructor

--- src/validators/min-length/min-length-validation.spec.ts
+++ src/validators/min-length/min-length-validation.spec.ts
@@ -4,19 +4,19 @@ import faker from "faker";
 
 describe('Min Lenght', () => {
 	test('Should return error if field is an empty', () => {
-		const sut = new MinLenghtValidation("email")
+		const sut = new MinLenghtValidation("email", 3)
 		const error = sut.validate('')
 		expect(error).toEqual(new InvalidFieldError())
 	})
 
 	test('Should return error if field have less than 3', () => {
-		const sut = new MinLenghtValidation("email")
+		const sut = new MinLenghtValidation("email", 3)
 		const error = sut.validate('ab')
 		expect(error).toEqual(new InvalidFieldError())
 	})
 
 	test('Should not return error because the field lenght is greather than 3', () => {
-		const sut = new MinLenghtValidation("email")
+		const sut = new MinLenghtValidation("email", 3)
 		const error = sut.validate(faker.internet.email())
 		expect(error).toBeFalsy()
 	})
--- src/validators/min-length/min-length-validation.ts
+++ src/validators/min-length/min-length-validation.ts
@@ -2,10 +2,10 @@ import { InvalidFieldError } from "../errors"
 import { FieldValidation } from "../protocols/field-validation"
 
 export class MinLenghtValidation implements FieldValidation {
-  constructor(readonly field: string) { }
+  constructor(readonly field: string, private readonly minlenght: number) { }
 
   validate(value: string) {
-    return value.length > 3 ? null : new InvalidFieldError()
+    return value.length > this.minlenght ? null : new InvalidFieldError()
   }
 }
 
